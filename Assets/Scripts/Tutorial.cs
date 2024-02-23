c6d90f7f3f31aed3a8d13a1e80db5461a16fc3a3
Paul De La Porte Des Vaux <paul.de-la-porte-des-vaux@epita.fr>
2024-02-23 15:39:29 +0100

    added tutoria at beginning

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// using textmesh pro
+using TMPro;
+
+public class Tutorial : MonoBehaviour
+{
+
+    public TextMeshProUGUI tutorialText;
+    public TextMeshProUGUI dialogSkip;
+    
+    
+    void Start() {
+        StartCoroutine(displayTutorialText());
+    }
+    
+    public IEnumerator displayTutorialText() {
+    
+        tutorialText.text = "Welcome to the tutorial! Press the arrow keys to move around.";
+        yield return new WaitForSeconds(0.5f);
+        dialogSkip.gameObject.SetActive(true);
+        while (!Input.GetKeyDown(KeyCode.RightArrow) && !Input.GetKeyDown(KeyCode.Return) && !Input.GetKeyDown(KeyCode.Space)) {
+            yield return null;
+        }
+
+        dialogSkip.gameObject.SetActive(false);
+        tutorialText.text = "Press the space bar to jump. (B)";
+        yield return new WaitForSeconds(0.5f);
+        dialogSkip.gameObject.SetActive(true);
+        while (!Input.GetKeyDown(KeyCode.RightArrow) && !Input.GetKeyDown(KeyCode.Return) && !Input.GetKeyDown(KeyCode.Space)) {
+            yield return null;
+        }
+
+        dialogSkip.gameObject.SetActive(false);
+        tutorialText.text = "Press the pageDown key to control your fox. (Y)";
+        yield return new WaitForSeconds(0.5f);
+        dialogSkip.gameObject.SetActive(true);
+        while (!Input.GetKeyDown(KeyCode.RightArrow) && !Input.GetKeyDown(KeyCode.Return) && !Input.GetKeyDown(KeyCode.Space)) {
+            yield return null;
+        }
+
+        dialogSkip.gameObject.SetActive(false);
+        tutorialText.text = "To enter a door, press the up arrow key.";
+        yield return new WaitForSeconds(0.5f);
+        dialogSkip.gameObject.SetActive(true);
+        while (!Input.GetKeyDown(KeyCode.RightArrow) && !Input.GetKeyDown(KeyCode.Return) && !Input.GetKeyDown(KeyCode.Space)) {
+            yield return null;
+        }
+
+        dialogSkip.gameObject.SetActive(false);
+        tutorialText.text = "Avoid the adults and snakes, and collect the roses. Have fun !";
+        yield return new WaitForSeconds(0.5f);
+        dialogSkip.gameObject.SetActive(true);
+        while (!Input.GetKeyDown(KeyCode.RightArrow) && !Input.GetKeyDown(KeyCode.Return) && !Input.GetKeyDown(KeyCode.Space)) {
+            yield return null;
+        }
+
+        gameObject.SetActive(false);
+    }
+}
